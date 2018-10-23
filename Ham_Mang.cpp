c054d4bcfefa7ed9df505f32d6fdacc923e4d8f3
trantrunghieu18it4 <44363235+trantrunghieu18it4@users.noreply.github.com>
2018-10-23 14:38:53 +0700

    Add files via upload

---
+++ Ham_Mang.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void Nhapmang(int a[100],int n)
+{
+	for (int i=0;i<n;i++)
+	 {
+	   printf("a[%d]= ",i);
+	   scanf("%d",&a[i]);
+    }
+}
+int Tong(int a[100],int n)
+{
+	int tong=0;
+	for (int i=0;i<n;i++)
+	   tong=tong+a[i]; 
+         return tong;	 
+}
+
+int Max(int a[100],int n)
+{
+     int max=a[0];
+for (int i=0;i<n;i++)
+	 {
+	   if(max>a[i])
+	      max=a[i];
+	return max;
+             }
+}
+int main()
+{
+int mang[100];
+int n;
+printf("Nhap so phan tu cua mang:");
+scanf("%d",&n);
+Nhapmang(mang,n);	
+printf("\nTong cac phan tu =%d",Tong(mang,n));
+
+printf("\nSo lon nhat la: %d",Max(mang,n));
+}
